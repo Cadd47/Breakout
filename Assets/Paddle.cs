15a85afc331048c5d90a461f41ae4bb0fbbf0a97
Cadd47 <tcadd47@gmail.com>
2022-06-01 14:07:49 -0700

    no message

---
+++ Assets/Paddle.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float boundX = 2.25f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        var vel = rb.velocity;
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            vel.x = -speed;
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            vel.x = speed;
+        }
+        else
+        {
+            vel.x = 0;
+        }
+        rb.velocity = vel;
+
+        var pos = transform.position;
+        if (pos.x > boundX)
+        {
+            pos.x = boundX;
+        }
+        else if (pos.x < -boundX)
+        {
+            pos.x = -boundX;
+        }
+        transform.position = pos;
+    }
+}
